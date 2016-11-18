34681b0da533cf8c37a6d20553ed8e15f5108bd7
Lino <luizgustavolino@gmail.com>
2016-11-18 18:04:07 -0200

    primeiro commit

---
+++ app/js/unova.js
@@ -0,0 +1,174 @@
+// Projeto Unova
+// 
+
+Handlebars.registerHelper('push', function(context) {
+    return "home.pushViewController('"+context+"')";
+});
+
+
+var unova = {}
+
+// ---[ UNOVA NAV CONTROLLER ]---
+
+unova.nav = {
+
+	counter: 0,
+
+	makeControllerForTemplate: function(templateName, data, then){
+
+		var newController = document.createElement('div');
+		newController.className = "unova-nav viewing"
+		newController.templateName = templateName
+		newController.id = "unova-nav"+(unova.nav.counter++)
+
+		unova.tpl.compile(newController, data, function(div){
+			then(div)
+		})
+
+	},
+
+	makeNavigationController: function(){
+
+		var navigation = document.createElement('div');
+		navigation.className = "unova-nav-controller"
+		navigation.stack = []
+		
+		navigation.setRootController = function(templateName, data){
+			unova.nav.makeControllerForTemplate(templateName, data,
+				function(controller){
+					navigation.stack = [controller]
+					while (navigation.firstChild){
+						navigation.removeChild(navigation.firstChild);
+					}
+					navigation.appendChild(controller);
+				}
+			)
+		}
+
+		navigation.pushViewController = function(templateName, data){
+
+			// TODO cerulean.nav.setUserInteractionEnabled(false);
+			unova.nav.makeControllerForTemplate(templateName, data,
+				function(controller){
+
+					var prevController = navigation.stack.last()
+					navigation.stack.push(controller);
+					navigation.appendChild(controller);
+
+					unova.dom.removeClass(prevController, "viewing");
+					unova.dom.addClass(prevController, "pushed");
+
+					var backBtn = document.getElementById("back-button");
+					unova.dom.removeClass(backBtn, "hidden")
+					
+				}
+			)
+		}
+
+		navigation.popViewController = function(){
+
+
+			var poppedController = navigation.stack.pop()
+			var newTopController = navigation.stack.last()
+
+			unova.dom.removeClass(newTopController, "pushed");
+			unova.dom.addClass(newTopController, "viewing");
+
+			if(navigation.stack[0] == newTopController){
+				var backBtn = document.getElementById("back-button")
+				unova.dom.addClass(backBtn, "hidden");
+			}
+
+			poppedController.remove()
+
+		}
+
+		return navigation
+	}
+
+}
+
+// ---[ UNOVA LOG HELPER ]---
+
+unova.log = function(msg){
+	console.log(msg)
+}
+
+// ---[ UNOVA TEMPLATE PARSER ]---
+
+unova.tpl = {
+
+    precompiled_templates:{},
+
+	compile: function(divTag, data, then){
+
+		function display(){
+			divTag.innerHTML = unova.tpl.precompiled_templates[divTag.templateName](data)
+			then(divTag)
+		}
+
+		if(unova.tpl.precompiled_templates[divTag.templateName]){
+	        display();
+		}else{
+			
+	            var r = new XMLHttpRequest();
+	            var base = "templates/";
+	            var templateName = divTag.templateName
+	            var loc = base + templateName + ".template.html";
+	            
+	            unova.log("loading html template: " + loc);
+	            
+	            r.open("GET", loc, true);
+	            r.onreadystatechange = function () {
+	                if (r.readyState == 4 && r.status == 200){
+	                    unova.tpl.precompiled_templates[templateName] = Handlebars.compile(r.responseText);
+	                	display();
+	                }
+	            };
+
+	            r.send();
+		}
+	}
+}
+
+// ---[ UNOVA DOM ]---
+
+unova.dom = {}
+
+unova.dom.addClass = function(tag, className){
+	if(tag.className.indexOf(className) == -1){
+		tag.className += " " + className;
+	}
+}
+
+unova.dom.removeClass = function(tag, className) {
+	if(tag.className.indexOf(className) != -1){
+		var regex = new RegExp('(?:^|\\s)'+className+'(?!\\S)',"g");
+		tag.className = tag.className.replace( regex ,'')
+	}
+}
+
+
+// ---- [ UNOVA JS EXTENSIONS ] ---
+
+if (!Element.prototype.remove){
+	Element.prototype.remove = function() {
+    	this.parentElement.removeChild(this);
+	}
+}
+
+if (!NodeList.prototype.remove){
+	NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+	    for(var i = this.length - 1; i >= 0; i--) {
+	        if(this[i] && this[i].parentElement) {
+	            this[i].parentElement.removeChild(this[i]);
+	        }
+	    }
+	}
+}
+
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
